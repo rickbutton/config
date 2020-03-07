85a9d36168141d40f8493479551a56959fc74532
Rick Button <rick@button.dev>
2020-03-07 18:35:13 -0500

    discord

--- pkgs/geras.sh
+++ pkgs/geras.sh
@@ -1,3 +1,3 @@
 # tools
 brew install coreutils stow htop gnupg2 git neovim zsh tmux cmake cloc ripgrep
-brew cask install iterm2 visual-studio-code firefox evernote spotify whatsapp
+brew cask install iterm2 visual-studio-code firefox evernote spotify whatsapp discord
